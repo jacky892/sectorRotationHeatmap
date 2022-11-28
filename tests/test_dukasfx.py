71c9f683b95d8251bd31f5aa3e829c6ab2d05b43
jackylee <jacky892@gmail.com>
2022-11-28 11:08:44 +0000

    added test case for dukascopy fx

---
+++ tests/test_dukasfx.py
@@ -0,0 +1,22 @@
+import pytest
+import unittest
+import logging
+logging.basicConfig()
+logger=logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+import os,sys
+if not os.path.exists('workflow'):
+    sys.path.append('..')
+    os.chdir('..')
+
+def log(msg):
+    logger.info(msg)
+    
+class TestClass(unittest.TestCase):
+    def test_dukasfx(self):
+        from workflow.sectorRotationTraderWorkflow import  run_forex_worflow
+        all_perf_table, all_trades_df=run_forex_worflow(pred_date='20221121', use_dukas=True)
+                
+        assert(len(all_perf_table)>0)
+        assert(len(all_trades_df)>0)
+        #assert(all_perf_table.exposure_adjusted_annualized_gain.mean()>0)
